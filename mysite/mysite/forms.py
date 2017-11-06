f18b95664abf3ab6ee4ed21af04c7ce826a79166
Jesse Simpson <jesse.simpson36@gmail.com>
2017-11-05 19:22:46 -0500

    included satviks desired radio buttons

--- mysite/mysite/forms.py
+++ mysite/mysite/forms.py
@@ -10,8 +10,43 @@ DISEASE_CHOICES = (
     ('pain management', 'Pain Management'),
     ('pregnancy', 'Pregnancy'),
     ('weight loss programs', 'Weight Loss Programs'),
-
 )
+
+INN_MOOP_CHOICES = (
+    (0,'0-100'),
+    (1,'101-500'),
+    (2,'501-2000'),
+    (3,'2001+'),
+)
+
+OON_MOOP_CHOICES = (
+    (0,'0-100' ),
+    (1,'101-1000' ),
+    (2,'1001-10000' ),
+    (3,'10000+'  ),
+)
+
+INN_DED_CHOICES = (
+    (0,'0-100' ),
+    (1,'101-500' ),
+    (2,'501-2000' ),
+    (3,'2001+'  ),
+)
+
+OON_DED_CHOICES = (
+    (0,'0-100' ),
+    (1,'101-1000'),
+    (2,'1001-10000'),
+    (3,'10000+'),
+)
+
+INN_COINSURANCE_CHOICES = (
+    (0,'0-25'),
+    (1,'26-50'),
+    (2,'51-75'),
+    (3,'76-100'),
+)
+
 
 class HealthInsuranceInputForm(forms.Form):
     # hard filters
@@ -21,5 +56,9 @@ class HealthInsuranceInputForm(forms.Form):
     # soft filters
     diseases = forms.MultipleChoiceField(choices=DISEASE_CHOICES, widget=forms.CheckboxSelectMultiple())
     desired_premium_price = forms.IntegerField(label='Desired Premium Price')
-    out_of_pocket_maximum = forms.IntegerField(label='Desired Out of Pocket Maximum')
-    deductible = forms.IntegerField(label='Desired Deductible')
+    desired_in_network_out_of_pocket_maximum =        forms.ChoiceField(choices=INN_MOOP_CHOICES, widget=forms.RadioSelect())
+    desired_out_of_network_out_of_pocket_maximum =        forms.ChoiceField(choices=OON_MOOP_CHOICES, widget=forms.RadioSelect())
+    desired_in_network_deductible =         forms.ChoiceField(choices=INN_DED_CHOICES, widget=forms.RadioSelect())
+    desired_out_of_network_deductible =         forms.ChoiceField(choices=OON_DED_CHOICES, widget=forms.RadioSelect())
+    desired_in_network_coinsurance = forms.ChoiceField(choices=INN_COINSURANCE_CHOICES, widget=forms.RadioSelect())
+
