45b6c06400597757f15ec1768ff0be0ea25423b1
jannelouisea <janne.louise.a@gmail.com>
2017-11-09 13:55:54 -0500

    Able to pass values from form to python script

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,9 +1,13 @@
+import sys
+sys.path.append('..')
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseNotFound
 from django.http import HttpResponseRedirect
 
 from .forms import HealthInsuranceInputForm
+from utils.example import functionEx
 
 
 # Create your views here.
@@ -14,6 +18,9 @@ def input(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
+
+            functionEx(form.cleaned_data.get('age'), form.cleaned_data.get('zipcode'))
+
             return render(request, 'results.html', {'form': form})
 
     # if a GET (or any other method) we'll create a blank form
